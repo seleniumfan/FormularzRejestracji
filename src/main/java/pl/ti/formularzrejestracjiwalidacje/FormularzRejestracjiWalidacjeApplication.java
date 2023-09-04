d955ed0065f4c8de3f6e951d97bdd2a990994bc9
Tomasz Iwanicki <tiko35@o2.pl>
2023-09-04 18:08:56 +0200

    Initial commit

---
+++ src/main/java/pl/ti/formularzrejestracjiwalidacje/FormularzRejestracjiWalidacjeApplication.java
@@ -0,0 +1,13 @@
+package pl.ti.formularzrejestracjiwalidacje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormularzRejestracjiWalidacjeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormularzRejestracjiWalidacjeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/ti/formularzrejestracjiwalidacje/FormularzRejestracjiWalidacjeApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ti.formularzrejestracjiwalidacje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormularzRejestracjiWalidacjeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
